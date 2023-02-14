6199250e9a342ed7c6467e9741acfc941a879a83
Salem Janeb <sjane913@gmail.com>
2023-02-14 19:50:45 +0100

    First msg

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+const http = require('http') ;
+const fs = require('fs') ;
+const fsPromises = require('fs').promises ;
 const logEvents = require('./logEvents') ;
 const EventEmitter = require('events') ; 
 
 class MyEvent extends EventEmitter {};
 
 const myEvent = new MyEvent() ; 
-myEvent.on('log',(msg)=>{logEvents(msg)}) ;
+// myEvent.on('log',(msg)=>{logEvents(msg)}) ;
 
-setTimeout(()=>{
-    myEvent.emit('log','Event Loged') ;
-},2000);
+
+// myEvent.emit('log','Event Loged') ;
