49e4e1e1ff43b14f9599861aadeabdcdb44e1f6e
Salem Janeb <sjane913@gmail.com>
2023-02-14 23:44:50 +0100

    v2

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const EventEmitter = require('events') ;
 class MyEvent extends EventEmitter {};
 
 const myEvent = new MyEvent() ; 
+const PORT = process.env.PORT || 3500 ;
+const server = http.createServer((req,res)=> {
+    console.log(req.url,req.method) ;
+})
+server.listen(PORT,()=> console.log(`Server running on port ${PORT}`)) ;
 // myEvent.on('log',(msg)=>{logEvents(msg)}) ;
 
 
